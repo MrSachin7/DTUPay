4243448e58a6304334a6962424828ecba75fc469
MrSachin07 <nepalghi@gmail.com>
2025-01-21 18:04:39 +0100

    Written some tests

--- account-service/src/main/java/eventConsumer/RegisterCustomerRequestProcessor.java
+++ account-service/src/main/java/eventConsumer/RegisterCustomerRequestProcessor.java
@@ -10,32 +10,30 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 @ApplicationScoped
 public class RegisterCustomerRequestProcessor {
 
     private final AccountService accountService;
 
-    @Inject
-    @Channel("RegisterCustomerCompleted")
-    Emitter<RegisterCustomerCompleted> registerCustomerCompletedEmitter;
-
     public RegisterCustomerRequestProcessor(AccountService accountService) {
         this.accountService = accountService;
     }
 
     @Incoming("RegisterCustomerRequested")
-    public void process(JsonObject obj){
+    @Outgoing("RegisterCustomerCompleted")
+    public RegisterCustomerCompleted process(JsonObject obj){
         System.out.println("Processing request to register customer");
         RegisterCustomerRequested event = obj.mapTo(RegisterCustomerRequested.class);
         try {
             String id = accountService.registerAccount(event.getFirstname(), event.getLastname(), event.getCprNumber(), event.getAccountNumber());
             System.out.println("Customer registered with id: " + id);
-            registerCustomerCompletedEmitter.send(new RegisterCustomerCompleted(event.getCoRelationId(),id, null));
+            return new RegisterCustomerCompleted(event.getCoRelationId(),id, null);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            registerCustomerCompletedEmitter.send(new RegisterCustomerCompleted(event.getCoRelationId(),null, e.getMessage()));
+            return new RegisterCustomerCompleted(event.getCoRelationId(),null, e.getMessage());
         }
     }
 
