899d3c08dc4bc0997cea8f41be1417f994ebd8bc
MrSachin07 <nepalghi@gmail.com>
2025-01-23 18:01:49 +0100

    save changes

--- facade/src/main/java/org/acme/resource/MerchantResource.java
+++ facade/src/main/java/org/acme/resource/MerchantResource.java
@@ -6,27 +6,33 @@ import jakarta.ws.rs.core.Response;
 import org.acme.dto.GenerateReportsResponse;
 import org.acme.dto.RegisterCustomerRequest;
 import org.acme.dto.RegisterCustomerResponse;
+import org.acme.dto.StartPaymentRequest;
+import org.acme.service.PaymentService;
 import org.acme.service.RegisterService;
 import org.acme.service.ReportService;
 import org.acme.service.UnregisterService;
 
-@Path("/customers")
-public class CustomerResource {
-    private final RegisterService registerService;
-    private final UnregisterService unregisterService;
+@Path("/merchants")
+public class MerchantResource {
+
+    private final PaymentService paymentService;
     private final ReportService reportService;
 
-    public CustomerResource(RegisterService registerService,
-                            UnregisterService unregisterService, ReportService reportService) {
+    private final RegisterService registerService;
+
+    private final UnregisterService unregisterService;
+
+    public MerchantResource(PaymentService paymentService, ReportService reportService, RegisterService registerService, UnregisterService unregisterService) {
+        this.paymentService = paymentService;
+        this.reportService = reportService;
         this.registerService = registerService;
         this.unregisterService = unregisterService;
-        this.reportService = reportService;
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response registerCustomer(RegisterCustomerRequest registerCustomerRequest) {
+    public Response registerMerchant(RegisterCustomerRequest registerCustomerRequest) {
         try {
             RegisterCustomerResponse response = registerService.register(registerCustomerRequest);
             return Response.ok(response).build();
@@ -35,26 +41,39 @@ public class CustomerResource {
         }
     }
 
+    @POST
+    @Consumes("application/json")
+    @Path("/{merchantId}/payments")
+    public Response pay(@PathParam("merchantId") String merchantId , StartPaymentRequest paymentRequested) {
+        try {
+            String paymentId = paymentService.startPayment(merchantId, paymentRequested);
+            return Response.ok(paymentId).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
     @GET
     @Consumes("application/json")
-    @Path("/{customerId}/reports")
-    public Response getReportsForCustomer(@PathParam("customerId") String customerId) {
+    @Path("/{merchantId}/reports")
+    public Response getReportsForMerchant(@PathParam("merchantId") String merchantId) {
         try {
-            GenerateReportsResponse reportsForAllPayments = reportService.getReportsForCustomer(customerId);
+            GenerateReportsResponse reportsForAllPayments = reportService.getReportsForMerchant(merchantId);
             return Response.ok(reportsForAllPayments).build();
         } catch (Exception e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         }
     }
-  
+
     @DELETE
-    @Path("/{customerId}")
-    public Response unregisterCustomer(@PathParam("customerId") String customerId) {
+    @Path("/{merchantId}")
+    public Response unregisterMerchant(@PathParam("merchantId") String merchantId) {
         try {
-            unregisterService.unregisterCustomer(customerId);
+            unregisterService.unregisterCustomer(merchantId);
             return Response.ok().build();
         } catch (Exception e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         }
     }
+
 }
